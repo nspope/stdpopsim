ee5aa5a1ddb5893a8d6c53d411b063d46be974dc
Nate Pope <nspope@utexas.edu>
2022-12-16 14:14:04 -0800

    Updated data

--- stdpopsim/catalog/EucPru/genetic_maps.py
+++ stdpopsim/catalog/EucPru/genetic_maps.py
@@ -5,12 +5,13 @@ _species = stdpopsim.get_species("EucPru")
 _gm = stdpopsim.GeneticMap(
     species=_species,
     id="Pope_PSUv1",
-    description="Genetic map inferred by pyrho with eight haploid bees from Puebla, Mexico",
+    description="Genetic map inferred by pyrho with eight "
+    "haploid bees from Puebla, Mexico",
     long_description="""
     FILLME
     """,
-    url="https://sesame.uoregon.edu/~natep/eucpru-data/hapmap/"
-    "eucera_pruinosa.pyrho.hapmap.tar.gz",
+    url="https://github.com/nspope/eucpru-popsim-data/raw/"
+    "eucera_pruinosa.genetic_map.tar.gz",
     sha256="807d3a5ea13909a7dd9cbf815dfeae9aee15c6fc9d95c5a1b031b936998c64b4",
     file_pattern="{id}.MEX.hapmap",
     citations=[
